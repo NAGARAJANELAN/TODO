b5662244941054b5ef3574d80de68c7f2b0e87ce
NAGARAJAN E <98300427+NAGARAJANELAN@users.noreply.github.com>
2022-11-26 16:44:15 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,62 @@
+const add = document.getElementById("add");
+
+add.addEventListener("click", addTask);
+
+okNode=null;
+
+function addTask() {
+  if(!lastTaskOk()) return;
+
+  console.log("last node ok");
+
+  const parent = document.getElementById("container");
+
+  const child = document.createElement("div");
+
+  const input = document.createElement("input");
+  input.type = "text";
+
+  // const ok = document.createElement("input");
+  // ok.type = "button";
+  // ok.value = "Ok";
+  // ok.setAttribute('id',"inputField");
+
+  const childDesc = document.createElement("span");
+
+  child.classList.add("taskList");
+
+  child.appendChild(input);
+  child.appendChild(okNode);
+  child.appendChild(childDesc);
+
+  parent.appendChild(child);
+}
+
+function lastTaskOk(){
+  const section = document.getElementsByClassName("taskList");
+
+  const lastSection = section[section.length - 1];
+
+  const lastInput = lastSection.lastElementChild.textContent;
+  console.log(lastInput);
+  if(lastInput==""){console.log("not k"); return false;}
+  return true;
+}
+
+document.getElementById("inputField").addEventListener("click", addTaskDesc);
+
+function addTaskDesc() {
+  console.log("in");
+  const section = document.getElementsByClassName("taskList");
+
+  const lastSection = section[section.length - 1];
+
+  const input = lastSection.firstElementChild.value;
+
+  lastSection.lastElementChild.innerHTML = input;
+
+  lastSection.removeChild(lastSection.firstElementChild);
+  // lastSection.firstElementChild.style.display="none";
+  okNode=lastSection.removeChild(lastSection.firstElementChild);
+  // lastSection[1].style.display='none';
+}
